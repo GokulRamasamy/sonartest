898552535112b794cb6704f9a793e8ff66d3acf5
gokulkannan <gokulkannan.r@raster.in>
2018-03-19 13:07:18 +0530

    code changes

--- src/main/java/com/sonar/test/sonartest/resource/Test.java
+++ src/main/java/com/sonar/test/sonartest/resource/Test.java
@@ -1,6 +1,7 @@
 package com.sonar.test.sonartest.resource;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Test {
 
-    @RequestMapping(value="/api/test")
+    @GetMapping(value="/api/test")
     public ResponseEntity<String> capitalize(@RequestParam("param1") String text){
 
         String capital = "Welcome to "+text.toUpperCase();
+        System.out.println(capital);
         return ResponseEntity.ok(capital);
 
     }
